b704164751f282af5979ca7b356583f3ff3c3aff
Yasith Silva <133279208+yasith46@users.noreply.github.com>
2025-08-15 18:00:22 +0530

    Split CloudStub.h to cpp and h

--- Milestone1/cpp/src/sim/CloudStub.h
+++ Milestone1/cpp/src/sim/CloudStub.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <random>
-#include <iostream>
 #include "InverterSim.h"
+
+#include <vector>
 
 /**
  * @class CloudStub
@@ -29,17 +26,7 @@ class CloudStub
          * @param   batch A vector containing the samples to upload.
          * @return `true` if the upload was successful, `false` if it failed.
          */
-        bool upload(const std::vector<Sample>& batch) 
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            if (rand01_() < 0.05)
-            {
-                std::cout << "Upload failed — re-queue for next window\n";
-                return false;
-            }
-            // Prefer single ACK line from coordinator; return true silently.
-            return true;
-        }
+        bool upload(const std::vector<Sample>& batch);
     
     private:
          /**
@@ -51,10 +38,5 @@ class CloudStub
           *
           * @return A random number in the range [0.0, 1.0].
           */
-        double rand01_() 
-        {
-            static thread_local std::mt19937_64 rng{std::random_device{}()};
-            static thread_local std::uniform_real_distribution<double> dist(0.0,1.0);
-            return dist(rng);
-        }
+        double rand01_();
 };
