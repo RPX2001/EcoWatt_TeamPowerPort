d01c130f4dacea91300d81b19c352026f5db2812
Raveen Pramuditha <raveenpspathirana99@gmail.com>
2025-08-29 17:24:38 +0530

    update milestone 2 and platformIO firmware

---
+++ build/CMakeFiles/ecowatt.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/core/application/Coordinator.cpp" "CMakeFiles/ecowatt.dir/src/core/application/Coordinator.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/core/application/Coordinator.cpp.obj.d"
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/core/peripheral/Acquisition.cpp" "CMakeFiles/ecowatt.dir/src/core/peripheral/Acquisition.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/core/peripheral/Acquisition.cpp.obj.d"
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/core/peripheral/Timers.cpp" "CMakeFiles/ecowatt.dir/src/core/peripheral/Timers.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/core/peripheral/Timers.cpp.obj.d"
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/core/peripheral/Uploader.cpp" "CMakeFiles/ecowatt.dir/src/core/peripheral/Uploader.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/core/peripheral/Uploader.cpp.obj.d"
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/main.cpp" "CMakeFiles/ecowatt.dir/src/main.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/main.cpp.obj.d"
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/sim/CloudStub.cpp" "CMakeFiles/ecowatt.dir/src/sim/CloudStub.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/sim/CloudStub.cpp.obj.d"
+  "D:/Work/Semester 7/Embedded Systems/EcoWatt_TeamPowerPort/Milestone1/cpp/src/sim/InverterSim.cpp" "CMakeFiles/ecowatt.dir/src/sim/InverterSim.cpp.obj" "gcc" "CMakeFiles/ecowatt.dir/src/sim/InverterSim.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
