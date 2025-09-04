55c157b0ac92ea6494e44d091aca1b069d0c6a0e
Raveen Pramuditha <raveenpspathirana99@gmail.com>
2025-09-04 21:31:08 +0530

    Uodate:Comments

--- Milestone 2/Ecowatt_device/src/main.cpp
+++ Milestone 2/Ecowatt_device/src/main.cpp
@@ -20,6 +20,20 @@ void setup() {
 void loop() {
   // Your main program logic
   // choose registers to poll
+/*
+  {REG_VAC1, 0, "Vac1"},
+  {REG_IAC1, 1, "Iac1"},
+  {REG_FAC1, 2, "Fac1"},
+  {REG_VPV1, 3, "Vpv1"},
+  {REG_VPV2, 4, "Vpv2"},
+  {REG_IPV1, 5, "Ipv1"},
+  {REG_IPV2, 6, "Ipv2"},
+  {REG_TEMP, 7, "Temp"},
+  {REG_POW,  8, "Pow"},
+  {REG_PAC,  9, "Pac"}
+
+*/
+
   const RegID selection[] = {REG_VAC1, REG_IAC1, REG_IPV1, REG_IPV2};
 
   // poll inverter
