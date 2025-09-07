5a7908c17af216a1930d2aca05eb1186fffc15df
Raveen Pramuditha <raveenpspathirana99@gmail.com>
2025-09-07 14:51:28 +0530

    Update:Comments

--- Milestone 2/Ecowatt_device/src/main.cpp
+++ Milestone 2/Ecowatt_device/src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
 #include "aquisition.h"
 #include "protocol_adapter.h"
+/*
+  Acquisition Module
+  Handles Modbus RTU frame construction and parsing
+  Uses ProtocolAdapter to send 
+
+choose registers to poll
+  {REG_VAC1, 0, "Vac1"},
+  {REG_IAC1, 1, "Iac1"},
+  {REG_FAC1, 2, "Fac1"},
+  {REG_VPV1, 3, "Vpv1"},
+  {REG_VPV2, 4, "Vpv2"},
+  {REG_IPV1, 5, "Ipv1"},
+  {REG_IPV2, 6, "Ipv2"},
+  {REG_TEMP, 7, "Temp"},
+  {REG_POW,  8, "Pow"},
+  {REG_PAC,  9, "Pac"}
+
+*/
 
 
 void setup() {
   Serial.begin(115200);
-  // adapter.setSSID("Raveenpsp");
-  // adapter.setPassword("raveen1234");
-  // adapter.setApiKey("NjhhZWIwNDU1ZDdmMzg3MzNiMTQ5YTFmOjY4YWViMDQ1NWQ3ZjM4NzMzYjE0OWExNQ==");
-  // adapter.begin();
   
-  // // Example write
-  // adapter.writeRegister("1106000800100B54");
-
-  // // Example read
-  // adapter.readRegister("110300000002C69B");
-  // Registers need to read from InverterSIM
   const RegID selection[] = {REG_VAC1, REG_IAC1, REG_IPV1, REG_PAC};
 
   // poll inverter
@@ -36,21 +44,5 @@ void setup() {
 }
 
 void loop() {
-  // choose registers to poll
-/*
-  {REG_VAC1, 0, "Vac1"},
-  {REG_IAC1, 1, "Iac1"},
-  {REG_FAC1, 2, "Fac1"},
-  {REG_VPV1, 3, "Vpv1"},
-  {REG_VPV2, 4, "Vpv2"},
-  {REG_IPV1, 5, "Ipv1"},
-  {REG_IPV2, 6, "Ipv2"},
-  {REG_TEMP, 7, "Temp"},
-  {REG_POW,  8, "Pow"},
-  {REG_PAC,  9, "Pac"}
-
-*/
-  
-  
 
 }
