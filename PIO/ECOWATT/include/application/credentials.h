31ae63da15a9a573df4419b041c1e2a24cd3d291
Raveen Pramuditha <raveenpspathirana99@gmail.com>
2025-10-24 22:04:11 +0530

    Update:added fault recovery

--- PIO/ECOWATT/include/application/credentials.h
+++ PIO/ECOWATT/include/application/credentials.h
@@ -4,9 +4,9 @@
 #warning: Check WiFi and server credentials in credentials.h before compiling!
 
 // Wifi Credentials
-#define WIFI_SSID "YasiruDEX"
-#define WIFI_PASSWORD "freewifi"
+#define WIFI_SSID "Raveenpsp"
+#define WIFI_PASSWORD "raveen1234"
 
-#define FLASK_SERVER_URL "http://192.168.1.141:5001"
+#define FLASK_SERVER_URL "http://10.189.206.28:5001"
 
 #endif // CREDENTIALS_H
